7b07aae4725360d97754f6ca9ca5abeec125936c
despc <idespc@gmail.com>
2023-11-11 09:34:36 +0300

    fix gachi med

--- SentisGameplayImprovements/Tweaks/MedkitPatches.cs
+++ SentisGameplayImprovements/Tweaks/MedkitPatches.cs
@@ -1,15 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Reflection;
-using NAPI;
+﻿using System.Reflection;
 using NLog;
-using Sandbox;
 using Sandbox.Common.ObjectBuilders.Definitions;
-using Sandbox.Game;
 using Sandbox.Game.Entities.Character;
-using Sandbox.Game.World;
 using SpaceEngineers.Game.EntityComponents.GameLogic;
 using Torch.Managers.PatchManager;
-using VRage.ModAPI;
 
 namespace SentisGameplayImprovements
 {
@@ -33,62 +27,26 @@ namespace SentisGameplayImprovements
         {
             if (!__instance.Entity.IsWorking)
                 return false;
-            bool flag = false;
             var character = __instance.User;
             if (character == null)
             {
                 character = user;
-                var propertyInfo = typeof(MyLifeSupportingComponent).GetProperty("User",
-                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-                propertyInfo.SetValue(__instance, user);
-                // __instance.User = user;
                 if (__instance.Entity.RefuelAllowed)
                 {
-                    user.SuitBattery.ResourceSink.TemporaryConnectedEntity = (IMyEntity) __instance.Entity;
-                    user.SuitBattery.RechargeMultiplier = 1;
-                    __instance.RechargeSocket.PlugIn(user.SuitBattery.ResourceSink);
-
                     var characterInventory = character.GetInventoryBase();
                     foreach (var item in characterInventory.GetItems())
                     {
                         if (item.Content is MyObjectBuilder_GasContainerObject)
                         {
+                            if (((MyObjectBuilder_GasContainerObject)item.Content).GasLevel > 1.0)
+                            {
+                                continue;
+                            }
                             ((MyObjectBuilder_GasContainerObject) item.Content).GasLevel = 1.02f;
                             characterInventory.OnContentsChanged();
                         }
                     }
-                    PlayerSuitRechargeEvent playerSuitRecharging = MyVisualScriptLogicProvider.PlayerSuitRecharging;
-                    if (playerSuitRecharging != null)
-                        playerSuitRecharging(character.GetPlayerIdentityId(), __instance.Entity.BlockType);
                 }
-            }
-            __instance.easySetField("m_lastTimeUsed", MySandboxGame.TotalGamePlayTimeInMilliseconds);
-            if (character.StatComp != null && __instance.Entity.HealingAllowed)
-            {
-                character.StatComp.DoAction("GenericHeal");
-                flag = true;
-                PlayerHealthRechargeEvent healthRecharging = MyVisualScriptLogicProvider.PlayerHealthRecharging;
-                if (healthRecharging != null)
-                {
-                    float num = character.StatComp.Health != null ? character.StatComp.Health.Value : 0.0f;
-                    healthRecharging(character.GetPlayerIdentityId(), __instance.Entity.BlockType, num);
-                }
-            }
-
-            return false;
-        }
-        
-        private static bool CheckLimitsAndNotifyPatch(long ownerID,
-            string blockName,
-            int pcuToBuild,
-            int blocksToBuild,
-            int blocksCount,
-            Dictionary<string, int> blocksPerType, ref bool __result)
-        {
-            if (MySession.Static.Players.IdentityIsNpc(ownerID))
-            {
-                __result = true;
-                return false;
             }
             return true;
         }
